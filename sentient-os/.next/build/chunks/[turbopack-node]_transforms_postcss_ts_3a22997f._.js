82edb83874b07b25a59e89bf88ff936de60966d1
Seungwon Lee <97718938+Heisnotanimposter@users.noreply.github.com>
2026-02-16 00:51:55 +0900

    Create [turbopack-node]_transforms_postcss_ts_3a22997f._.js

---
+++ sentient-os/.next/build/chunks/[turbopack-node]_transforms_postcss_ts_3a22997f._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Linux22/sentient-os/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/a35a8_cc534db1._.js",
+  "chunks/[root-of-the-server]__d5a1126a._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Linux22/sentient-os/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
