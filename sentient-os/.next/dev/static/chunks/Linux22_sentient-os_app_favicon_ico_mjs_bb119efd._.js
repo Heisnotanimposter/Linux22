3c2ab1ddca99dadad7cf3383f000b9d2252ce652
Seungwon Lee <97718938+Heisnotanimposter@users.noreply.github.com>
2026-03-02 16:25:33 +0900

    Create Linux22_sentient-os_app_favicon_ico_mjs_bb119efd._.js

---
+++ sentient-os/.next/dev/static/chunks/Linux22_sentient-os_app_favicon_ico_mjs_bb119efd._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/a35a8_next_dist_0192d767._.js"
+],
+    source: "dynamic"
+});
